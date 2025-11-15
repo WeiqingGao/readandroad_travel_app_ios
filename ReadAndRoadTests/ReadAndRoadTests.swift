327e8743408801c7494a180a50589ce6d27ad524
Weiqing Gao <wkinggao@gmail.com>
2025-11-15 15:01:54 -0500

    Initial Commit

---
+++ ReadAndRoadTests/ReadAndRoadTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadAndRoadTests.swift
+//  ReadAndRoadTests
+//
+//  Created by Weiqing Gao on 11/15/25.
+//
+
+import Testing
+@testable import ReadAndRoad
+
+struct ReadAndRoadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
